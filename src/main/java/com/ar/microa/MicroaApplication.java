186a0c08bf2fa15897458902e817ba8aff2be3fe
adithyan2206 <adithyan2206@gmail.com>
2022-01-23 22:05:42 +0530

    modified

--- src/main/java/com/ar/microa/MicroaApplication.java
+++ src/main/java/com/ar/microa/MicroaApplication.java
@@ -2,12 +2,20 @@ package com.ar.microa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroaApplication.class, args);
 	}
 
+	
+	
+	
 }
